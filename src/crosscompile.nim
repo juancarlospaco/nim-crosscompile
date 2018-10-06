d150065e0f0c45b6c00991e6cdefc2dc69e43a3e
juancarlospaco <juancarlospaco@gmail.com>
2018-10-05 21:09:28 -0300

    Add option to enable or disable build targets

--- src/crosscompile.nim
+++ src/crosscompile.nim
@@ -12,15 +12,15 @@ type CompileResult* = tuple[
 
 
 # TODO:   (all boolean parameters)
-# Enable/Disable Compilation for windows/linux/android/javascript
-# Enable/Disable Documentation
-# Enable/Disable ZIP
-# Enable/Disable Strip
 # Replace linux sha1sum with a Nim function
 # Remove hardcoded name
+# add to Hackpad
+# add to Telegram-Nim-Bot
 
 
-proc crosscompile*(code, target, opt, release, gc, app, ssls, threads: string): CompileResult =
+proc crosscompile*(code, target, opt, release, gc, app, ssls, threads: string,
+  build4windows=true, build4linux=true, build4android=true, build4javascript=true,
+  build4docs=true, use_zip=true, use_strip=true): CompileResult =
   ## Receives code as string and crosscompiles and generates HTML Docs, Strips and ZIPs.
   var win, winzip, winsha, lin, linzip, linsha, doc, doczip, logs, jsf, jszip, jssha, andr, andrzip, andrsha: string
   if countLines(code.strip) >= 1:
@@ -36,47 +36,50 @@ proc crosscompile*(code, target, opt, release, gc, app, ssls, threads: string): 
       output: string
       exitCode: int
     # Linux Compilation.
-    (output, exitCode) = execCmdEx(fmt"nim {target} {release} {opt} {gc} {app} {ssls} {threads} --out:{temp_file_bin} {temp_file_nim}")
-    logs &= output
-    if exitCode == 0:
-      (output, exitCode) = execCmdEx(fmt"{strip_cmd} {temp_file_bin}")
+    if build4linux:
+      (output, exitCode) = execCmdEx(fmt"nim {target} {release} {opt} {gc} {app} {ssls} {threads} --out:{temp_file_bin} {temp_file_nim}")
       logs &= output
+      if exitCode == 0 and use_strip:
+        (output, exitCode) = execCmdEx(fmt"{strip_cmd} {temp_file_bin}")
+        logs &= output
       if exitCode == 0:
         lin = splitPath(temp_file_bin).tail
         (output, exitCode) = execCmdEx(fmt"sha1sum {temp_file_bin}")
         logs &= output
-        if exitCode == 0:
-          linsha = output
-          var z: ZipArchive
-          discard z.open(temp_file_bin & ".zip", fmWrite)
-          z.addFile(temp_file_bin)
-          z.close
-          linzip = splitPath(temp_file_bin & ".zip").tail
+      if exitCode == 0 and use_zip:
+        linsha = output
+        var z: ZipArchive
+        discard z.open(temp_file_bin & ".zip", fmWrite)
+        z.addFile(temp_file_bin)
+        z.close
+        linzip = splitPath(temp_file_bin & ".zip").tail
     # Windows Compilation.
-    (output, exitCode) = execCmdEx(fmt"nim {target} {release} {opt} {gc} {app} {ssls} {threads} --cpu:amd64 --os:windows {windows_args} --out:{temp_file_exe} {temp_file_nim}")
-    logs &= output
-    if exitCode == 0:
-      (output, exitCode) = execCmdEx(fmt"{strip_cmd} {temp_file_exe}")
+    if build4windows:
+      (output, exitCode) = execCmdEx(fmt"nim {target} {release} {opt} {gc} {app} {ssls} {threads} --cpu:amd64 --os:windows {windows_args} --out:{temp_file_exe} {temp_file_nim}")
       logs &= output
+      if exitCode == 0 and use_strip:
+        (output, exitCode) = execCmdEx(fmt"{strip_cmd} {temp_file_exe}")
+        logs &= output
       if exitCode == 0:
         win = splitPath(temp_file_exe).tail
         (output, exitCode) = execCmdEx(fmt"sha1sum {temp_file_exe}")
         logs &= output
-        if exitCode == 0:
-          winsha = output
-          var z: ZipArchive
-          discard z.open(temp_file_exe & ".zip", fmWrite)
-          z.addFile(temp_file_exe)
-          z.close
-          winzip = splitPath(temp_file_exe & ".zip").tail
+      if exitCode == 0 and use_zip:
+        winsha = output
+        var z: ZipArchive
+        discard z.open(temp_file_exe & ".zip", fmWrite)
+        z.addFile(temp_file_exe)
+        z.close
+        winzip = splitPath(temp_file_exe & ".zip").tail
     # JavaScript Compilation.
-    (output, exitCode) = execCmdEx(fmt"nim js -d:nodejs {release} {opt} --out:{temp_file_js} {temp_file_nim}")
-    logs &= output
-    if exitCode == 0:
-      jsf = splitPath(temp_file_js).tail
-      (output, exitCode) = execCmdEx(fmt"sha1sum {temp_file_js}")
+    if build4javascript:
+      (output, exitCode) = execCmdEx(fmt"nim js -d:nodejs {release} {opt} --out:{temp_file_js} {temp_file_nim}")
       logs &= output
       if exitCode == 0:
+        jsf = splitPath(temp_file_js).tail
+        (output, exitCode) = execCmdEx(fmt"sha1sum {temp_file_js}")
+        logs &= output
+      if exitCode == 0 and use_zip:
         jssha = output
         var z: ZipArchive
         discard z.open(temp_file_js & ".zip", fmWrite)
@@ -84,32 +87,35 @@ proc crosscompile*(code, target, opt, release, gc, app, ssls, threads: string): 
         z.close
         jszip = splitPath(temp_file_js & ".zip").tail
     # Android Compilation.
-    (output, exitCode) = execCmdEx(fmt"nim c --os:android {release} {opt} {android_args} --out:{temp_file_andr} {temp_file_nim}")
-    logs &= output
-    if exitCode == 0:
-      (output, exitCode) = execCmdEx(fmt"{strip_cmd} {temp_file_andr}")
+    if build4android:
+      (output, exitCode) = execCmdEx(fmt"nim c --os:android {release} {opt} {android_args} --out:{temp_file_andr} {temp_file_nim}")
       logs &= output
-      if exitCode == 0:
-        andr = splitPath(temp_file_andr).tail
-        (output, exitCode) = execCmdEx(fmt"sha1sum {temp_file_andr}")
+      if exitCode == 0 and use_strip:
+        (output, exitCode) = execCmdEx(fmt"{strip_cmd} {temp_file_andr}")
         logs &= output
         if exitCode == 0:
-          andrsha = output
-          var z: ZipArchive
-          discard z.open(temp_file_andr & ".zip", fmWrite)
-          z.addFile(temp_file_andr)
-          z.close
-          andrzip = splitPath(temp_file_andr & ".zip").tail
+          andr = splitPath(temp_file_andr).tail
+          (output, exitCode) = execCmdEx(fmt"sha1sum {temp_file_andr}")
+          logs &= output
+      if exitCode == 0 and use_zip:
+        andrsha = output
+        var z: ZipArchive
+        discard z.open(temp_file_andr & ".zip", fmWrite)
+        z.addFile(temp_file_andr)
+        z.close
+        andrzip = splitPath(temp_file_andr & ".zip").tail
     # HTML Docs Generation.
-    (output, exitCode) = execCmdEx(fmt"nim doc --out:{temp_file_html} {temp_file_nim}")
-    logs &= output
-    if exitCode == 0:
-      doc = splitPath(temp_file_html).tail
-      var z: ZipArchive
-      discard z.open(temp_file_html & ".zip", fmWrite)
-      z.addFile(temp_file_html)
-      z.close
-      doczip = splitPath(temp_file_html & ".zip").tail
+    if build4docs:
+      (output, exitCode) = execCmdEx(fmt"nim doc --out:{temp_file_html} {temp_file_nim}")
+      logs &= output
+      if exitCode == 0 and use_zip:
+        doc = splitPath(temp_file_html).tail
+        var z: ZipArchive
+        discard z.open(temp_file_html & ".zip", fmWrite)
+        z.addFile(temp_file_html)
+        z.close
+        doczip = splitPath(temp_file_html & ".zip").tail
+
   let resultaditos: CompileResult = (
     win: win, winzip: winzip, winsha: winsha.strip, lin: lin, linzip: linzip,
     linsha: linsha.strip, doc: doc, doczip: doczip, logs: logs, jsf: jsf,
